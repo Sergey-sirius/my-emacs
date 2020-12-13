eb15fc2ca892f10304eaaf95706c71ebcce45d31
sirius <2hotab2@gmail.com>
2020-12-14 01:00:18 +0200

    new1

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -1,21 +1,25 @@
-;; --------------------------
+;; =================
 ;; Прописываем ErgoEmacs MODE
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-
 (add-to-list 'load-path "~/.emacs.d/ergoemacs-mode")
 (require 'ergoemacs-mode)
 (setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
 (setq ergoemacs-keyboard-layout "ru") ;; Assumes QWERTY keyboard layout
 (ergoemacs-mode 1)
+;;
+(eval-when-compile(require 'cl))
+(require 'package)
+(setq package-archives nil)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/") t)
+(setq package-enable-at-startup nil)
+(package-initialize nil)
 ;; --------------------------
 
-;;
-;; ----- Фикс для работы комбинаций клавиш на русской раскладке
+;; ========== Фикс для работы комбинаций клавиш на русской раскладке
 (defun reverse-input-method (input-method)
   "Build the reverse mapping of single letters from INPUT-METHOD."
   (interactive
@@ -44,63 +48,44 @@
     ad-do-it))
 (reverse-input-method 'russian-computer)
 ;; --------------------------
+
+;;============== Theme
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (whiteboard)))
  '(ergoemacs-keyboard-layout "ru")
  '(package-selected-packages
    (quote
     (org-beautify-theme org-attach-screenshot org-analyzer org-alert org-agenda-property org-ac org))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
+;;--------------------------
 
-;;
-;; ------ Инверсия выделения
+;; =============== Инверсия выделения
 (setq-default transient-mark-mode t)
 ;; --------------------------
 
-;;
-;; load both themes
-(load-theme 'brutalist t t)
-(load-theme 'brutalist-dark t t)
+;; ================== Шрифты
+(set-face-attribute 'default nil :height 110)
+(when (member "DejaVu Sans Mono" (font-family-list))
+  (set-face-attribute 'default nil :font "DejaVu Sans Mono"))
+;; ----------------------------------
 
-(require 'solar)
+;; ========= TODO my list
+;;(setq org-agenda-files (list "~/main/myrepo/ToDo/work.org"
+;;                             "~/main/myrepo/ToDo/myproject.org" 
+;;                             "~/main/myrepo/ToDo/home.org"))
 
-;; set these to your current location
-(setq calendar-latitude [47 22 north]
-      calendar-longitude [8 33 east])
+;; =====================
+;;; САМЫЙ ВАЖНЫЙ МОМЕНТ - ВЫНЕСЕНИЕ НАСТРОЕК СРЕДЫ В settings.el и загрузка их оттуда
+(setq custom-file "~/.emacs.d/settings.el")
+(load-file "~/.emacs.d/settings.el")
+;; ------------------
 
-(defun enable-appropriate-brutalist-theme ()
-  "Enable the brutalist or brutalist-dark theme
-depending on the time of day."
-  (interactive)
-  (let* ((date (calendar-current-date))
-     (data (solar-sunrise-sunset date))
-     (sunrise (car (car data)))
-     (sunset (car (cadr data)))
-     (time (decode-time))
-     (now (+ (nth 2 time) (/ (nth 1 time) 60.0))))
-    (cond ((< now sunrise)
-       (message "%dh and %02dmin to go until sunrise"
-	    (truncate (- sunrise now))
-	    (round (mod (* 60 (- sunrise now)) 60))))
-      ((< now sunset)
-       (message "%dh and %02dmin to go until sunset"
-	    (truncate (- sunset now))
-	    (round (mod (* 60 (- sunset now)) 60))))
-      (t
-       (message "%dh and %02dmin to go until sunrise"
-	    (truncate (- (+ 24 sunrise) now))
-	    (- 60 (round (mod (* 60 (- (+ 24 sunrise) now)) 60))))))
-    (if (and (< sunrise now) (< now sunset))
-    (enable-theme 'brutalist)
-      (enable-theme 'brutalist-dark))))
 
-;; and do it
-(enable-appropriate-brutalist-theme)
+;;=======================================================================
+(use-package beacon
+  :diminish
+  :commands beacon-mode)
+
+
+;; --------------------------------
